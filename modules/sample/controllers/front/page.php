5abbd6925f209d78d7c7ba099c746b2bb6cefbb5
Web Development Labs <webdevlabs@users.noreply.github.com>
2017-03-13 17:56:41 +0200

    Update page.php

--- modules/sample/controllers/front/page.php
+++ modules/sample/controllers/front/page.php
@@ -11,18 +11,18 @@
 namespace Modules\Sample\Controllers\Front;
 use System;
 
-class Sample extends System {
+class Page extends System {
 	private $tpl_dir;
 
 public function __construct($registry) {
 	parent::__construct($registry);
-	$this->tpl_dir = ROOT_DIR.'/modules/pages/views/front/'; // enable if you want tpl inside mod views
+	$this->tpl_dir = ROOT_DIR.'/modules/sample/views/front/'; // enable if you want tpl inside mod views
 }
 
 public function index () {
 	$request=$this->url->getRequest();
 	if (!$this->template->isCached($this->tpl_dir.'page.tpl')) {
-		$page_array = $this->pages->fetch_page('uri',$request[1]);
+		$page_array = $this->sample->fetch_page('uri',$request[1]);
 		$this->template->assign('page',$page_array);
 		$this->template->assign('title', $page_array["title"]);
 	}
