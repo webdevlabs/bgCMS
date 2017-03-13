b8ea80e82081f953e79e27a8c650c7f8b3bf30d7
Web Development Labs <webdevlabs@users.noreply.github.com>
2017-03-13 18:16:31 +0200

    Update cfg.php

--- modules/sample/cfg.php
+++ modules/sample/cfg.php
@@ -6,11 +6,11 @@ return array(
 	'version'=>'1.0',
 	'author'=>'Simeon Lyubenov (office@webdevlabs.com)',
 	'uri'=>'sample',
-	'front_tpl_files'=>array("sample.tpl"),
-	'admin_tpl_files'=>array(),
-	'admin_js_files'=>array(),
-	'front_js_files'=>array(),
-	'admin_css_files'=>array('sample.css'),
-	'front_css_files'=>array('sample.css')
+	'front_tpl_files'=>["page.tpl"],
+	'admin_tpl_files'=>[],
+	'admin_js_files'=>[],
+	'front_js_files'=>[],
+	'admin_css_files'=>[],
+	'front_css_files'=>['sample.css']
 );
 ?>
